76837910bbc1025c9208109a8d6020f3e6457c90
TedDay5 <144728376+TedDay5@users.noreply.github.com>
2024-09-23 20:03:17 -0600

    First commit

--- code.js
+++ code.js
@@ -1,3 +1,22 @@
 function divideAndConquerSum(a) {
-    return a;
+    return sumOfArrays(a, 0, a.length-1);
+}
+
+function sumOfArrays(a, head, tail) {
+    if (head > tail) {
+        return 0;
+    }
+    if (head === tail) {
+        return a[head];
+    }
+
+    const split = Math.floor((tail - head + 1)/3);
+    const thrid1 = head + split;
+    const thrid2 = head + (split*2);
+
+    const subA1 = sumOfArrays(a, head, thrid1-1);
+    const subA2 = sumOfArrays(a, thrid1, thrid2-1);
+    const subA3 = sumOfArrays(a, thrid2, tail);
+
+    return subA1 + subA2 + subA3;
 }
